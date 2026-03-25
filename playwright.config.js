efe41fd2950896045810c21daba824ba813f3109
Congying Dai <congying@usc.edu>
2026-03-25 15:40:29 -0700

    added test cases in web/codec.spec.js

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   timeout: 30000,
   use: { baseURL: 'http://127.0.0.1:5173', headless: true },
   webServer: {
-    command: 'python3 -m http.server 5173 --directory web',
+    command: 'python -m http.server 5173 --directory web',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: true,
   },
